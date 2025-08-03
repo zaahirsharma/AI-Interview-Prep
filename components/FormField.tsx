48f038f7703f55d4dab92215f6f6b36e32e572b5
Zaahir Sharma <sharma.zaahir@gmail.com>
2025-08-02 23:58:46 -0500

    Fix FormField

--- components/FormField.tsx
+++ components/FormField.tsx
@@ -1,5 +1,4 @@
-import React from 'react'
-import {FormControl, FormDescription, FormItem, FormLabel, FormMessage} from "@/components/ui/form";
+import {FormControl, FormItem, FormLabel, FormMessage} from "@/components/ui/form";
 import {Input} from "@/components/ui/input";
 import {Control, Controller, FieldValues, Path} from "react-hook-form";
 
@@ -11,23 +10,28 @@ interface FormFieldProps<T extends FieldValues>{
     placeholder?: string;
     type?: "text" | "email" | "password";
 }
-const FormField = ({control, name, label, placeholder, type="text" }: FormFieldProps<T>) => (
-    <Controller
-        name={name}
-        control={control}
-        render={({ field }) => (
-            <FormItem>
-                <FormLabel className={"label"}>Username</FormLabel>
-                <FormControl>
-                    <Input placeholder="shadcn" {...field} />
-                </FormControl>
-                <FormDescription>
-                    This is your public display name.
-                </FormDescription>
-                <FormMessage />
-            </FormItem>
-        )}
-    />
-);
+
+const FormField = <T extends FieldValues>({control, name, label, placeholder, type="text" }: FormFieldProps<T>) => {
+    return (
+        <Controller
+            control={control}
+            name={name}
+            render={({ field }) => (
+                <FormItem>
+                    <FormLabel className="label">{label}</FormLabel>
+                    <FormControl>
+                        <Input
+                            className="input"
+                            type={type}
+                            placeholder={placeholder}
+                            {...field}
+                        />
+                    </FormControl>
+                    <FormMessage />
+                </FormItem>
+            )}
+        />
+    );
+};
 
 export default FormField
