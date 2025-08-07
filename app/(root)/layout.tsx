5977fbb3e1bf30deb7876a9b9219b4c9bf2667f2
Zaahir Sharma <sharma.zaahir@gmail.com>
2025-08-06 22:38:12 -0500

    Start homepage

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,8 +1,20 @@
-import React, {ReactNode} from 'react'
+import Link from "next/link";
+import Image from "next/image";
+import {ReactNode} from "react";
 
 const RootLayout = ({children}: {children: ReactNode}) => {
     return (
-        <div>{children}</div>
+        <div className="root-layout">
+            <nav>
+                <Link href={"/"} className={"flex items-center gap-2"}>
+                    <Image src={"/logo.svg"} alt={"Logo"} width={38} height={32} />
+                    <h2 className={"text-primary-100"}>Synapse</h2>
+                </Link>
+            </nav>
+
+            {children}
+        </div>
     )
 }
+
 export default RootLayout
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import {Button} from "@/components/ui/button";
+import Link from "next/link";
+import Image from "next/image";
 
 const Page = () => {
     return (
-        <div>Home Page</div>
+        <>
+            <section className={"card-cta"}>
+                <div className={"flex flex-col gap-6 max-w-lg"}>
+                    <h2>Get Interview Ready With AI-Powered Practice & Feedback</h2>
+                    <p className={"text-lg"}>Practice with real interview questions & get instant feedback</p>
+                    {/*Button will be property of a child which will be a link*/}
+                    <Button asChild className={"btn-primary max-sm:w-full"}>
+                        <Link href={"/interview"}>Start an Interview</Link>
+                    </Button>
+                </div>
+
+                <Image
+                    src={"/robot.png"}
+                    alt={"robot"}
+                    width={"400"}
+                    height={"400"}
+                    className={"max-sm:hidden"}
+                />
+            </section>
+
+            <section>
+
+            </section>
+        </>
     )
 }
 export default Page
